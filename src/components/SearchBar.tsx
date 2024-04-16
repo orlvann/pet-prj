a07787708f72e2fd1308ea58724f0b1c86017d89
orlvann <orlovaanna641@gmail.com>
2024-04-17 01:16:05 +0200

    Add SearchBar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Input, InputGroup, InputLeftElement, Box } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+
+interface SearchBarProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => {
+  return (
+    <Box className='search'>
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents='none'
+          children={<SearchIcon color='gray.300' />}
+        />
+        <Input
+          type='text'
+          placeholder='Search'
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      </InputGroup>
+    </Box>
+  );
+};
+
+export default SearchBar;
