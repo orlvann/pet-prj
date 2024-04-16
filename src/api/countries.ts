835656b6c70d672c1f54c8d5df0e69c3b7b57ca0
orlvann <orlovaanna641@gmail.com>
2024-04-17 00:59:23 +0200

    Add CountriesTable component

--- src/api/countries.ts
+++ src/api/countries.ts
@@ -1,19 +1,85 @@
 import { useQuery } from 'react-query';
 
-interface Country {
-  name: string;
-  population: number;
-  flag: string;
-  capital: string;
-  region: string;
+export interface Name {
+  common: string;
+  official: string;
+  nativeName: {
+    [key: string]: {
+      official: string;
+      common: string;
+    };
+  };
 }
 
-export const useCountries = () => {
-  return useQuery<Country[], Error>('countries', async () => {
-    const res = await fetch('https://restcountries.com/v3.1/region/europe');
-    if (!res.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return res.json();
-  });
+export interface Flags {
+  png: string;
+  svg: string;
+}
+
+export interface Country {
+  name: Name;
+  tld: string[];
+  cca2: string;
+  ccn3: string;
+  cca3: string;
+  cioc: string;
+  independent: boolean;
+  status: string;
+  unMember: boolean;
+  currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+  idd: {
+    root: string;
+    suffixes: string[];
+  };
+  capital: string[];
+  region: string;
+  subregion: string;
+  languages: {
+    [key: string]: string;
+  };
+  translations: {
+    [key: string]: {
+      official: string;
+      common: string;
+    };
+  };
+  latlng: number[];
+  landlocked: boolean;
+  area: number;
+  demonyms: {
+    [key: string]: {
+      f: string;
+      m: string;
+    };
+  };
+  flags: Flags;
+  population: number;
+}
+
+export const fetchEUCountries = async (): Promise<Country[]> => {
+  const response = await fetch('https://restcountries.com/v3.1/region/europe');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+export const useFetchEUCountries = () => {
+  return useQuery<Country[], Error>('countries', fetchEUCountries);
+};
+
+export const useFetchCountryDetail = (countryCode: string) => {
+  return useQuery<Country, Error>(
+    ['country', countryCode],
+    () =>
+      fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`).then((res) =>
+        res.json()
+      ),
+    { enabled: !!countryCode }
+  );
 };
--- src/pages/CountryDetail.tsx
+++ src/pages/CountryDetail.tsx
@@ -1,8 +1,90 @@
 import { useParams } from 'react-router-dom';
+import { Box, Image, Text, Spinner, Heading, VStack } from '@chakra-ui/react';
+import { useQuery } from 'react-query';
+import { Country } from '../api/countries';
 
-const CountryDetail = () => {
-  let { id } = useParams();
-  return <div>Country Detail Component for {id}</div>;
+const renderCountryDetail = (country: Country) => {
+  return Object.entries(country).map(([key, value]) => {
+    if (
+      value &&
+      typeof value === 'object' &&
+      !Array.isArray(value) &&
+      !(value instanceof Date)
+    ) {
+      return (
+        <Box key={key}>
+          <Heading as='h3' size='md'>
+            {key}
+          </Heading>
+          {renderCountryDetail(value)}
+        </Box>
+      );
+    }
+
+    if (Array.isArray(value)) {
+      return (
+        <Text key={key}>
+          {key}: {value.join(', ')}
+        </Text>
+      );
+    }
+
+    if (value instanceof Date) {
+      return (
+        <Text key={key}>
+          {key}: {value.toDateString()}
+        </Text>
+      );
+    }
+
+    return (
+      <Text key={key}>
+        {key}: {value.toString()}
+      </Text>
+    );
+  });
 };
 
-export default CountryDetail;
+export const CountryDetail = () => {
+  const { countryCode } = useParams<{ countryCode: string }>();
+
+  const {
+    data: countryData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery<Country[], Error>(
+    ['country', countryCode],
+    () =>
+      fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`).then((res) =>
+        res.json()
+      ),
+    {
+      enabled: !!countryCode,
+    }
+  );
+
+  const country = countryData && countryData[0];
+
+  if (isLoading) return <Spinner />;
+  if (isError || !country)
+    return (
+      <Box>An error occurred: {error?.message || 'Country not found'}</Box>
+    );
+
+  return (
+    <Box p={4}>
+      <Heading as='h2' size='xl' mb={4}>
+        {country.name.official}
+      </Heading>
+      <Image
+        src={country.flags.png}
+        alt={`Flag of ${country.name.common}`}
+        boxSize='100px'
+      />
+      <VStack align='start' spacing={4}>
+        {renderCountryDetail(country)}
+      </VStack>
+    </Box>
+  );
+};
