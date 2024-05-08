c6a2965f74f726720e92350e0202296015d3f538
orlvann <orlovaanna641@gmail.com>
2024-05-08 18:49:44 +0200

    modified the chartbar to look good in dark mode

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -18,8 +18,11 @@ const CustomBarChart: React.FC<CustomBarChartProps> = ({ data }) => {
   const { colorMode } = useColorMode();
   const bgColor = colorMode === 'dark' ? 'gray.800' : 'white';
   const borderColor = colorMode === 'dark' ? 'gray.700' : 'gray.200';
-  const textColor = colorMode === 'dark' ? '#fff' : '#000';
-  const gridColor = '#e0e0e0';
+  const textColor = colorMode === 'dark' ? '#e0e0e0' : '#000';
+  const isDarkMode = colorMode === 'dark';
+  const gridColor = isDarkMode ? '#e0e0e0' : '#e0e0e0';
+
+  const fillId = isDarkMode ? 'whiteGradient' : 'colorfulGradient';
 
   const formatNumbers = (value: number) => {
     return `${(value / 1000000).toFixed(1)} million`;
@@ -38,9 +41,13 @@ const CustomBarChart: React.FC<CustomBarChartProps> = ({ data }) => {
           }}
         >
           <defs>
-            <linearGradient id='colorPopulation' x1='0' y1='0' x2='0' y2='1'>
+            <linearGradient id='whiteGradient' x1='0' y1='0' x2='0' y2='1'>
+              <stop offset='5%' stopColor='#cccccc' stopOpacity={0.8} />
+              <stop offset='95%' stopColor='#4A5568' stopOpacity={0.8} />
+            </linearGradient>
+            <linearGradient id='colorfulGradient' x1='0' y1='0' x2='0' y2='1'>
               <stop offset='5%' stopColor='#4A5568' stopOpacity={0.8} />
-              <stop offset='95%' stopColor='#4A5568' stopOpacity={0} />
+              <stop offset='95%' stopColor='#e0e0e0' stopOpacity={0.8} />
             </linearGradient>
           </defs>
           <CartesianGrid strokeDasharray='3 3' stroke={gridColor} />
@@ -50,19 +57,20 @@ const CustomBarChart: React.FC<CustomBarChartProps> = ({ data }) => {
             formatter={(value: number) =>
               `${(value / 1000000).toFixed(1)} million`
             }
-            labelStyle={{ color: textColor }}
-            itemStyle={{ color: textColor }}
+            labelStyle={{ color: '#333' }}
+            itemStyle={{ color: '#333' }}
             cursor={{ fill: 'transparent' }}
             contentStyle={{
-              color: textColor,
+              color: 'black',
               backgroundColor: '#ffffff',
               borderRadius: '10px',
               borderColor: 'gray',
               borderWidth: '1px',
               padding: '10px',
+              background: isDarkMode ? 'gray.400' : 'gray.200',
             }}
           />
-          <Bar dataKey='population' fill='url(#colorPopulation)' />
+          <Bar dataKey='population' fill={`url(#${fillId})`} />
         </BarChart>
       </ResponsiveContainer>
     </Box>
